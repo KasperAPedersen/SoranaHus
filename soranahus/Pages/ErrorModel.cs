2d2f0edf2a691b93d645d489782542a00c44ff1e
Kasper Doe <kasper@doe.dk>
2024-11-01 17:51:01 +0100

    :)

---
+++ soranahus/Pages/ErrorModel.cs
@@ -0,0 +1,9 @@
+﻿// File: Pages/ErrorModel.cs
+namespace soranahus.Pages
+{
+    public class ErrorModel
+    {
+        public string RequestId { get; set; }
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
